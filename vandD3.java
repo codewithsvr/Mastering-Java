0785f4e034261817bb11b162b6f878c55b593a08
Edurinty Shashivadhan Reddy <eshashi535@gmail.com>
2026-05-20 12:32:01 +0530

    Add files via upload

---
+++ vandD3.java
@@ -0,0 +1,24 @@
+/*Q3. Character Challenge
+
+Take one character input.
+
+Input:
+
+M
+
+Output:
+
+You entered: M*/
+
+import java.util.*;
+
+public class vandD3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter char");
+        char x=sc.next().charAt(0);
+        System.out.println("You entered: "+x);
+        
+    }
+    
+}
