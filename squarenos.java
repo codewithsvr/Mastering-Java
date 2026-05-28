f0165d3b645a49e6dbc15c79b1dd85c89b482bba
Edurinty Shashivadhan Reddy <eshashi535@gmail.com>
2026-05-28 21:55:32 +0530

    Add files via upload

---
+++ squarenos.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class squarenos {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("ENTER THE NUMBER:");
+        int n=sc.nextInt();
+        int Square=n*n;
+        System.out.println("SQUARE OF NUMBER IS:"+Square);
+        sc.close();
+    }
+} 
