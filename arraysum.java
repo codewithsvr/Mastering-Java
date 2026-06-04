33b9d5177ff6d9a052b8273ee5be60281ff2f48b
Edurinty Shashivadhan Reddy <eshashi535@gmail.com>
2026-06-04 12:37:11 +0530

    Add files via upload

---
+++ arraysum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class arraysum {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int array[]=new int[x];
+        for(int i=0;i<x;i++){
+            array[i]=sc.nextInt();
+        }
+        int sum=0;
+        for(int i=0;i<x;i++){
+            sum=sum+array[i];
+        }
+        System.out.println(sum);
+    }
+}
